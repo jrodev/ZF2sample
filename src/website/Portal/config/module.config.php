f47dc8221a38b4fc27430a2bf63438a3a3c2104e
Jaime Rodriguez <jrodev@yahoo.es>
2014-10-09 18:32:28 -0500

    update renders and config in protal

--- src/website/Portal/config/module.config.php
+++ src/website/Portal/config/module.config.php
@@ -83,10 +83,13 @@ return array(
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/layout'  => __DIR__ . '/../view/layout/layout.phtml',
+            'renders/_head'  => __DIR__ . '/../view/layout/renders/_head.phtml',
+            'renders/_header'=> __DIR__ . '/../view/layout/renders/_header.phtml',
+            'renders/_footer'=> __DIR__ . '/../view/layout/renders/_footer.phtml',
             'portal/index/index' => __DIR__ . '/../view/portal/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            'error/404'      => __DIR__ . '/../view/error/404.phtml',
+            'error/index'    => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
