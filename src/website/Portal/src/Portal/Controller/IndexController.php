ca9f45aea2b502a9b7fb9827093b0a155e2e370f
Jaime R <jrodev@yahoo.es>
2014-10-16 08:27:21 -0500

    add module chimp

--- src/website/Portal/src/Portal/Controller/IndexController.php
+++ src/website/Portal/src/Portal/Controller/IndexController.php
@@ -17,6 +17,9 @@ class IndexController extends AbstractActionController
     public function indexAction()
     {   
         flog('indexAction');
+        // https://github.com/zf-fr/zfr-mailchimp-module
+        $mailChimpClient = $serviceManager->get('ZfrMailChimp\Client\MailChimpClient');
+        
         $storeScript = $this->getServiceLocator()->get('storeScript');
         $storeScript->setStore('fooIndex',['item'=>2,'item2'=>array(1,2)]);
         $this->layout()->setTemplate("layout/layout");
